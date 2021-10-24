31eb83a416597ad9213ca6bb521f8911f2d8f500
andrewIntern <andrew@everapp.io>
2021-10-23 22:11:26 -0700

    Added Navigation Functionality
    All, active, and current filters dynamically change the Todo list seen. The selected button is hi-lighted green, and there is a blue hover effect.

--- todo/app.js
+++ todo/app.js
@@ -1,5 +1,19 @@
 //this is equivalent to ToDo.js
+//for hi-liting selected button
+let btns = document.getElementsByClassName('target');
+console.log('btns', btns);
+for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener('click', function () {
+        let current = document.getElementsByClassName('active');
+        current[0].className = current[0].className.replace(' active', '');
+        this.className += ' active';
+    });
+}
+
 //Selectors
+document.querySelector('#all').addEventListener('click', filterAll);
+document.querySelector('#active').addEventListener('click', filterActive);
+document.querySelector('#completed').addEventListener('click', filterCompleted);
 document.querySelector('form').addEventListener('submit', handleSubmitForm);
 document
     .querySelector('ul')
@@ -27,14 +41,15 @@ function addTodo(todo) {
 
     //be sure to close the elements to avoid duplicates <i> needs </i>
     li.innerHTML = `
-    <button name="checkButton"><i class="far fa-check-square"></i></button>
+    <button id="checkButton" name="checkButton"><i class="far fa-check-square"></i></button>
     <span class="todo-item">${todo}</span>
 
-    <button name="deleteButton"><i class="far fa-window-close"></i></button>
+    <button class="deleteButton" name="deleteButton"><i class="far fa-window-close"></i></button>
     `;
 
     li.classList.add('todo-list-item');
     ul.appendChild(li);
+    countTodo();
 }
 
 function checkTodo(e) {
@@ -42,9 +57,81 @@ function checkTodo(e) {
     if (item.style.textDecoration == 'line-through')
         item.style.textDecoration = 'none';
     else item.style.textDecoration = 'line-through';
+    countTodo();
 }
 
 function deleteTodo(e) {
     let item = e.target.parentNode;
     item.remove();
+    countTodo();
+}
+
+// TODO task is added or checked/unchecked or deleted call count function
+function filterActive(e) {
+    // alert('hello');
+    // console.log('e', e); //sometimes too complex
+    let todos = document.getElementsByClassName('todo-list-item');
+    //console.log('todos', todos);
+    console.log('todos', typeof todos);
+    for (todo of todos) {
+        console.log('todo', todo);
+        if (todo.style.textDecoration == 'line-through') {
+            todo.style.display = 'none';
+        } else if (todo.style.display == 'none') {
+            todo.style.display = 'block';
+        }
+    }
+}
+
+function filterCompleted(e) {
+    // alert('hello'); //use when only alert
+    // console.log('e', e); //sometimes too complex
+    let todos = document.getElementsByClassName('todo-list-item');
+    //console.log('todos', todos);
+    console.log('todos', typeof todos);
+    for (todo of todos) {
+        console.log('todo', todo);
+        if (todo.style.textDecoration != 'line-through') {
+            todo.style.display = 'none';
+        } else if (todo.style.display == 'none') {
+            todo.style.display = 'block';
+        }
+    }
+}
+
+function filterAll(e) {
+    // alert('hello'); //use when only alert
+    // console.log('e', e); //sometimes too complex
+    let todos = document.getElementsByClassName('todo-list-item');
+    //console.log('todos', todos);
+    console.log('todos', typeof todos);
+    for (todo of todos) {
+        // console.log('todo', todo);
+        if (
+            todo.style.textDecoration == 'line-through' ||
+            todo.style.display == 'none'
+        ) {
+            todo.style.display = 'block';
+        }
+    }
+    //bug from having countTodo in the conditions
+    // countTodo(); //not needed here
+}
+
+function countTodo() {
+    let count = 0;
+    let todos = document.getElementsByClassName('todo-list-item');
+    console.log('todo all', todos);
+    // console.log('todos', todos);
+    for (todo of todos) {
+        if (todo.style.textDecoration != 'line-through') {
+            count += 1;
+        }
+    }
+    //better to increment and make small steps
+    //sometimes the browser gets tired
+    //just double check code and refresh
+    let b = document.querySelector('b');
+    // console.log(b);
+    b.innerHTML = count;
 }
